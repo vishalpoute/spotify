cd6a7a0f46680b6594fdc7689085a0f72e4f77c4
vishu <vishupoutte154@gmail.com>
2024-09-23 18:51:08 +0530

    FireBase Setup Completed Successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import  'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,12 +8,18 @@ import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:spotify/presentation/splash/pages/splash.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
